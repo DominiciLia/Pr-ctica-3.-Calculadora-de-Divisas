bca95be6c8872069223319a472dfcb5d82538e70
Lia Karielys Torres Dominici <Dominicilia1@gmail.com>
2026-03-07 13:22:03 -0400

    Proyecto terminado y toques finales aplicados

--- Main.py
+++ Main.py
@@ -8,4 +8,21 @@ def Cambio_Moneda (DineroUsuario, ValorTasa) :
 print("Massih: Recien aterrizamos a Chile!")
 print("Maria:Dejame sacar la calculadora!")
 print("Maria: tenemos 350 dolares, eso hace:")
+print()
 print(Cambio_Moneda(350,910.71))
+
+
+print()
+print("Massih: Recien aterrizamos a Europa!")
+print("Maria:Dejame sacar la calculadora!")
+print("Maria: tenemos 600 dolares, eso hace:")
+print()
+print(Cambio_Moneda(600,0.86))
+
+print()
+print("Massih: Recien aterrizamos a Japon!")
+print("Maria:Dejame sacar la calculadora!")
+print("Maria: tenemos 455 dolares, eso hace:")
+print()
+print(Cambio_Moneda(455,157.79 ))
+print()
