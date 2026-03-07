8793a8c6b6e674b5eb6b95bb842799e54fc70b81
Lia Karielys Torres Dominici <Dominicilia1@gmail.com>
2026-03-07 13:12:55 -0400

    Creacion de la funcion

---
+++ Main.py
@@ -0,0 +1,11 @@
+
+def Cambio_Moneda (DineroUsuario, ValorTasa) :
+       Valorcambio = DineroUsuario * ValorTasa
+
+       return Valorcambio
+
+
+print("Massih: Recien aterrizamos a Chile!")
+print("Maria:Dejame sacar la calculadora!")
+print("Maria: tenemos 350 dolares, eso hace:")
+print(Cambio_Moneda(350,910.71))
